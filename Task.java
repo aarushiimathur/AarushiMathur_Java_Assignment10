126fbfa518a36ed8d95904e82dc5de317561ed1b
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-18 16:18:22 +0530

    adding comments

--- Task.java
+++ Task.java
@@ -1,19 +1,30 @@
+// Runnable allows the class to be executed by a thread.
 class Task implements Runnable {
+
+    // Declare a member variable to hold the name of the task.
     String name;
 
+    // Constructor to initialize the name of the task.
     Task(String name) {
         this.name = name;
     }
 
+    // Override the run() method which contains the code to be executed by the thread.
     public void run() {
+        // Loop 5 times to simulate a task doing repeated work.
         for (int i = 1; i <= 5; i++) {
+            // Print the current task name and the current iteration count.
             System.out.println(name + " is running: " + i);
+            
             try {
-                Thread.sleep(400);  // Pause to simulate work
+                // Pause the thread for 400 milliseconds to simulate some time-consuming task.
+                Thread.sleep(400);
             } catch (InterruptedException e) {
+                // Handle the case where the thread is interrupted during sleep.
                 System.out.println(name + " was interrupted.");
             }
         }
+        // Print a message when the task has finished running.
         System.out.println(name + " has finished.");
     }
 }
